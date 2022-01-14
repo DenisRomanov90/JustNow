7859a593f82f8dbfa1682d137f12bfff84e0f6bf
Denis Romanov <denrom90@gmail.com>
2022-01-14 09:06:53 +0300

    add

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    val den = Users("Den", 542452)
-    val ivan = Users("Ivan", 9854)
+    val den = Users("Den", 450)
+    val ivan = Users("Ivan", 39600)
     val katya = Users("Катя", 180000)
     den.wasAgo()
     ivan.wasAgo()
--- src/main/kotlin/Users.kt
+++ src/main/kotlin/Users.kt
@@ -1,21 +1,20 @@
 class Users (private var name : String, private val timeAgo: Int) {
 
     fun wasAgo() {
-        val minutes: Int = timeAgo / 60
-        val hours: Int = (timeAgo / 60)/60
+        val minutes: Int = timeAgo/60
+        val hours: Int = (timeAgo/60)/60
         when (timeAgo) {
             in 0..60 -> println("$name был(а) только что")
             in 61..(60 * 60) -> println("$name был(а) $minutes ${convertToMin()} назад")
             in (60 * 60) + 1..(24 * 60 * 60) -> println("$name был(а) $hours ${convertToHours()} назад")
-            in (24 * 60 * 60) + 1..(24 * 60 * 60 * 2) -> println("сегодня")
-            in (24 * 60 * 60 * 2) + 1..(24 * 60 * 60 * 3) -> println("вчера")
-            else -> println("давно")
+            in (24 * 60 * 60) + 1..(24 * 60 * 60 * 2) -> println("$name был(а) сегодня")
+            in (24 * 60 * 60 * 2) + 1..(24 * 60 * 60 * 3) -> println("$name был(а) вчера")
+            else -> println("$name был(а) давно")
         }
     }
 
     private fun convertToMin()=
         when ((timeAgo/60)%100) {
-            in 11..11 -> "минут"
             in 0..1 -> "минуту"
             in 2..4 -> "минуты"
             else -> "минут"
@@ -23,8 +22,7 @@ class Users (private var name : String, private val timeAgo: Int) {
 
 
     private fun convertToHours()=
-        when ((timeAgo/60)%100) {
-            in 11..11 -> "часов"
+        when (((timeAgo/60)/60)%100) {
             in 0..1 -> "час"
             in 2..4 -> "часа"
             else -> "часов"
